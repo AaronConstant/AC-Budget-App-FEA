308c843b39fce722b438fbc3261822b4ba578b39
AaronConstant <aconstant@pursuit.org>
2024-07-02 10:20:06 -0400

    testing faker library

--- BudgetFrontEnd/src/App.jsx
+++ BudgetFrontEnd/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import NavBar from './Components/NavBar'
 import { Routes, Route } from 'react-router-dom'
-import Show from './Components/Show'
+// import Show from './Components/Show'
+import Home from './Components/Home'
+import New from './Components/New'
 function App() {
 
 
@@ -11,8 +13,8 @@ function App() {
     <>
     <NavBar/>
     <Routes>
-      <Route path='/' element={<Home/>}/>
-
+      <Route path='/transactions' element={<Home/>}/>
+      <Route path='transactions/new' element={<New/>}/>
     </Routes>
     </>
   )
--- BudgetFrontEnd/src/Components/Home.jsx
+++ BudgetFrontEnd/src/Components/Home.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { faker } from '@faker-js/faker'
+
 export default function Home() {
+    const API = import.meta.env.VITE_BASE_URL
+    const [transactions, setTransactions] = useState([])
+    useEffect(() => {
+        fetch(API)
+        .then(res => res.json())
+        .then(res => {
+            setTransactions(res)
+        })
+        .catch(err => console.error(err))
 
+    },[])
 
+    const randomName = faker.person.firstName()
+    console.log(randomName);
   return (
-    <div></div>
+    <div>
+        {transactions.map((item, i) => { 
+            return( <div key = { i }>
+                { item.name }
+            </div>)
+        })}
+    </div>
   )
 }
